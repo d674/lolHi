bf9b020139d12b0c34d605edb23b62adafd28642
d674 <>
2020-10-18 17:50:58 +0900

    수정

---
+++ db.sql
@@ -0,0 +1,36 @@
+# 데이터 베이스 생성
+DROP DATABASE IF EXISTS lolHiT;
+CREATE DATABASE lolHiT;
+USE lolHiT;
+
+# 게시물 테이블 생성
+CREATE TABLE article (
+    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    regDate DATETIME NOT NULL,
+    updateDate DATETIME NOT NULL,
+    title CHAR(200) NOT NULL,
+    `body` TEXT NOT NULL
+);
+
+# 게시물 데이터 생성
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목1',
+`body` = '내용1';
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목2',
+`body` = '내용2';
+
+# 게시물 테이블 생성
+CREATE TABLE `member` (
+    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    regDate DATETIME NOT NULL,
+    updateDate DATETIME NOT NULL,
+    loginId CHAR(20) NOT NULL,
+    loginPw CHAR(100) NOT NULL,
+    `name` CHAR(100) NOT NULL
+);
